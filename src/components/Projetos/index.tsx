be7aca20a0ec6d5ec5773666f2171a5918e1484a
Felipe Tajima <felipebtajima05@gmail.com>
2026-04-10 23:37:13 -0300

    Adicionando Imagens aos cards de projeto

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,6 +1,11 @@
 import { Container } from '../Container'
 import ProjectCard from '../ProjectCard'
-import DevBurger from '../../hamb-1.png'
+import TaskList from '../../assets/images/tasklist.jpg'
+import CloudCar from '../../assets/images/cloudcar.jpg'
+import DevBurguer from '../../assets/images/devburguer.jpg'
+import TodayMovies from '../../assets/images/todaymovies.jpg'
+import DevLink from '../../assets/images/devlink.jpg'
+import WebCarros from '../../assets/images/webcarros.jpg'
 
 function Projetos() {
 
@@ -17,8 +22,8 @@ function Projetos() {
               <ProjectCard
                 title="Task List"
                 description="Site para organizar tarefas, o usuário pode criar tarefas, editá-las, marcar como concluída ou cancelada e depois listar por concluídas, em andamento ou canceladas."
-                image={DevBurger}
-                skills={["HTML", "Tailwind.css", "Javascript"]}
+                image={TaskList}
+                skills={["Javascript", "Tailwind.css", "HTML"]}
                 githubLink="https://github.com/vulgotaj/Task-List-JS"
                 projectLink="https://task-list-js-delta.vercel.app/"
               />
@@ -26,7 +31,7 @@ function Projetos() {
               <ProjectCard
                 title="Cloud Car"
                 description="Landing page que simula uma loja de peças automotivas. O projeto foi feito com finalidade de praticar conceitos de estilização CSS, logo, não há muitas funcionalidades em Javascript."
-                image={DevBurger}
+                image={CloudCar}
                 skills={["HTML", "CSS", "Javascript"]}
                 githubLink="https://github.com/vulgotaj/CloudCar_LandingPage"
                 projectLink="https://cloud-car-landing-page.vercel.app/"
@@ -34,9 +39,9 @@ function Projetos() {
 
               <ProjectCard
                 title="Dev Burguer"
-                description="Landing page que simula uma loja de peças automotivas. O projeto foi feito com finalidade de praticar conceitos de estilização CSS, logo, não há muitas funcionalidades em Javascript."
-                image={DevBurger}
-                skills={["HTML", "Tailwind.css", "Javascript"]}
+                description="Site para loja fictícia de hamburgueres, onde tem sistema de horário de funcionamento funcional, além de carrinho funcional que termina enviando uma mensagem ao número da loja com os detalhes do pedido e preço total."
+                image={DevBurguer}
+                skills={["Javascript", "Tailwind.css", "HTML"]}
                 githubLink="https://github.com/vulgotaj/DevBurguer"
                 projectLink="https://dev-burguer-nu.vercel.app/"
               />
@@ -44,17 +49,26 @@ function Projetos() {
               <ProjectCard
                 title="todayMOVIES"
                 description="Site onde é possível ver os filmes que estão em cartaz nos cinemas. Além disso, você pode ver seus detalhes (gênero, nota, duração, etc.), acessar os trailers no youtube e também adicionar a uma listagem de favoritos."
-                image={DevBurger}
+                image={TodayMovies}
                 skills={["React.js", "CSS"]}
                 githubLink="https://github.com/vulgotaj/today-movies"
                 projectLink="https://today-movies.vercel.app/"
               />
 
               <ProjectCard
+                title="DevLink"
+                description="Site inspirado no Linketree onde se pode manipular os elementos a fim de criar seus próprios links e personalizar fonte e background como quiser."
+                image={DevLink}
+                skills={["React.js", "Typescript", "Firebase", "Tailwind.css"]}
+                githubLink="https://github.com/vulgotaj/devlink"
+                projectLink="https://devlink-xi.vercel.app/"
+              />
+
+              <ProjectCard
                 title="Web Carros"
                 description="Projeto fictício de um site de compra e venda de carros, onde o usuário pode criar e logar em sua conta para realizar a compra, ou mesmo o anúncio de seu próprio carro para venda."
-                image={DevBurger}
-                skills={["React.js", "Typescript", "Redux", "Tailwind.css"]}
+                image={WebCarros}
+                skills={["React.js", "Typescript", "Firebase", "Tailwind.css"]}
                 githubLink="https://github.com/vulgotaj/WebCarros"
                 projectLink="https://web-carros-nu.vercel.app/"
               />
