c054c2e12daf0bce107cf9953ff6abd4a8c99a73
Felipe Tajima <felipebtajima05@gmail.com>
2026-04-12 02:53:44 -0300

    Ajustando tamanho do skill card

--- src/components/SkillCard/index.tsx
+++ src/components/SkillCard/index.tsx
@@ -7,7 +7,7 @@ interface SkillCardProps {
 
 function SkillCard({ title, icon }: SkillCardProps) {
     return (
-        <div className="h-60 w-45 flex flex-col gap-7 justify-center items-center text-sky-400 border-sky-400 border-2 rounded-lg transition-all duration-500 hover:-rotate-6 hover:border-sky-300 hover:text-sky-300">
+        <div className="h-60 w-40 flex flex-col gap-7 justify-center items-center text-sky-400 border-sky-400 border-2 rounded-lg transition-all duration-500 hover:-rotate-6 hover:border-sky-300 hover:text-sky-300">
           {icon &&
             <div className="text-7xl">
               {icon}
