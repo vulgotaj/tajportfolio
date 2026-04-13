651d82af36a03e609378c2173e8c9fd13a3a2af5
Felipe Tajima <felipebtajima05@gmail.com>
2026-04-13 14:43:10 -0300

    Adicionando melhorias de desempenho pt.2

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource/poppins/200.css'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/700.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
